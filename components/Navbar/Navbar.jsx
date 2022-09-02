543f0ece331e96420207512b6fc84c3d2abb703a
Adam Baker <adam_baker@live.co.uk>
2022-09-02 20:02:36 +0100

    nav bar complete

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 // Images
 import Logo from "../../public/assets/MM_Logo.png";
 import { FaBars } from "react-icons/fa";
+import akaLogo from "../../public/akaLogo.svg";
 
 const Navbar = ({ toggle }) => {
   const router = useRouter();
@@ -15,8 +16,15 @@ const Navbar = ({ toggle }) => {
   return (
     <div className={styles.nav}>
       <div className={styles.nav__preHeader}>
-        <p>AKA Automotive</p>
-        <p>01274&nbsp;583903</p>
+        <div className={styles.nav__akaLogo}>
+          <Image
+            src={akaLogo}
+            alt="aka automotive logo"
+            width="204"
+            height="34px"
+          />
+        </div>
+        <p className={styles.nav__phoneNo}>01274&nbsp;583903</p>
       </div>
       <div className={styles.nav__logo}>
         <Link href="/">
