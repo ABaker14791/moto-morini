469a992b6bae9da939bea25cfb9ff2918a53083c
Adam Baker <adam_baker@live.co.uk>
2022-09-07 22:20:49 +0100

    map components added to contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -2,12 +2,13 @@ import React from "react";
 // Styles
 import styles from "../styles/VisitUs.module.scss";
 // Components
+import Map from "../components/Map/ContactMap";
 import ContactForm from "../components/ContactForm/ContactForm";
 
 const contact = () => {
   return (
     <div className={styles.container}>
-      MAP
+      <Map />
       <ContactForm />
     </div>
   );
