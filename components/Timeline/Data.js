749ed56d6b466206ede839d58f8d65f41a76ca20
Adam Baker <adam_baker@live.co.uk>
2022-09-17 20:48:31 +0100

    timeline component added and map through data file for each node

---
+++ components/Timeline/Data.js
@@ -0,0 +1,117 @@
+export const timelineItems = [
+  {
+    date: "1937",
+    title: "1937: The start of the adventure",
+    body: "After almost 15 years of collaboration with Mario Mazzetti and the MM, in September 1937 Alfonso Morini sets up on his own. He opens a small firm in via Malvasia in Bologna where in January 1938 he starts to produce three-wheelers with 350, 500 and 600 cc engine sizes. A choice dictated by the conditions of the period as motorcycle type three-wheelers enjoy reduced taxation, a driving license is not necessary and they cost a third of small trucks. Affordability and good performance begin to give the new company in Bologna notoriety. For the technical aspect Dante Lambertini, Gino Marchesini and Dolcino Veronesi are involved, who will become key figures in the post-war period when the company will concentrate on the motorcycle sector, abandoning three-wheelers.",
+  },
+  {
+    date: "1946",
+    title: "1946: The first motorbike",
+    body: "Notwithstanding the wartime bombardments which destroyed the factory, Alfonso Morini does not lose heart and he starts again from scratch. In the 2-stroke 125 engine he sees the perfect vehicle suitable for postwar needs. The first Moto Morini even appears in spring 1946, and due to time constraints it is inspired by the best 125 of the time, the DKW RT. Production starts in a new factory in via Ludovico Berti, always in Bologna. The 125 is the first lightweight engine to be produced in an Italy liberated from fascism and when it is presented to the trade fair in Milan it is defined as motorcycling’s “success of the year”.",
+  },
+  {
+    date: "1947",
+    title: "1947",
+    body: "The tourist model is flanked by the sports version. Power increases from 4.5 HP to 5.7 HP, speed from 75 to 80 km/h. The first 125 models top their rivals for quality, reliability and comfort (even from its debut it is equipped with rear suspension). Production ends in 1954. With the two-stroke 125 starts the first race competitions and the first victories are achieved. Umberto Mascetti starts off his brilliant career with this small motorcycle which thrills for the first time, especially in 1948 when winning several races in the second tier Italian championship. The race version has 4 gears instead of 3 and arrives at 8/9 HP. Speed is around 120 km/h.",
+  },
+  {
+    date: "1949",
+    title: "1949",
+    body: "Realising the superiority of the 4-stroke engine, Alfonso Morini gives the green light to a new GP motorcycle, the chain-driven 125 single-shaft . Compressed 9:1 and with a 28mm carburetor, it produces 12 HP at 10.000 rpm. The motorbike weighs 80 kg. Besides several top tier national titles, this is the first Moto Morini to win a GP, in 1951 at Monza with Emilio Mendogni, beating the MV and the Mondial bialbero bikes. At the end it is able to produce 16 HP.",
+  },
+  {
+    date: "1953",
+    title: "1953",
+    body: "The 175 Settebello debuts, the top of the range of the Bologna firm, much sought after also by those who want to compete in races, especially long distance. This model, developed with competition in mind, achieved its maximum level of development in 1962 with the 22 HP at 10.500 rpm “Aste Corte” version.",
+  },
+  {
+    date: "1954",
+    title: "1954",
+    body: "At the year of the year the fabulous 175 Settebello is created, destined for the Milano-Taranto and the Motogiro d’Italia races. A single-cylinder with single-shaft split distribution capable of 22 HP at 9.000 rpm. It has a very effective chassis – an open duplex cradle frame, top level hydraulic suspensions – and it proves to be a winner in both the classic long-distance races. Produced in around 15 examples, it has also been developed as a 250 cc.",
+  },
+  {
+    date: "1955",
+    title: "1955",
+    body: "The increase in production requires a new plant and so Moto Morini moves to via Bergami 7, of course always in Bologna.",
+  },
+  {
+    date: "1957",
+    title: "1957",
+    body: "Alfonso Morini decides that it is now the moment to take part with continuity in the world speed championship and the new project for a 250 cc GP bike is developed by Nerio Biavati, the ex-right hand man of the technician Alfonso Drusiani in the corse Mondial department. Entrusted to Mendogni it will go on to win the Shell Cup in Imola in 1959, but later it will not be able to compete with the MVs. The demise of the 2-stroke 125 causes Moto Morini to fill the gap in the range in the more commercial market. Rejecting the hypothesis of a reduced 175 single-cylinder, a new more modern and rational product is chosen, in such a way as to reduce the number of components and to so limit production costs. Hence the creation of the nippy 100 – an engine size still adequate at the time – which will serve as a base two years later for the birth of the Corsaro 125.",
+  },
+  {
+    date: "1964",
+    title: "1964",
+    body: "Another great rider whose name is linked to Moto Morini is Angelo Bergamonti (18 March 1939 – 4 April 1971). An admirer of test rides with which he shares great affinity: the passion and respect for the mechanics, research and riding style. Debuting in 1957 in the reserves category with a 175 Settebello, and after a pause of a few years also due to marriage, he restarts as a junior in 1964, always on a Morini 175 which he prepares in the Gussola workshop. In 1966 he is a senior, racing with the Corsaro 125 and the glorious 250 bialbero ex-test bike which gives Moto Morini victory in the 1967 Italian championship.",
+  },
+  {
+    date: "1965",
+    title: "1965",
+    body: "Exports to the USA begin through the importer John Berti. New models are created with different specifications from those sold in Italy and they even change name: for example the Corsarino becomes the Pirate and Twister, the Corsaro, always depending on the version is called the Thunder Chief, Jaguar, and Hurricane.",
+  },
+  {
+    date: "1966",
+    title: "1966",
+    body: "Even if the first true open road 125 was born in 1962, deriving from the road model from which it distinguishes itself by its few details, we need to wait until 1965 to see the first real Corsaro off road in action. From 1966 the victories start: in the 125 category Moto Morini wins the two-day Bologna and the Bergamo valleys events and the Road Race trophy. Moreover, Franco Dall’Ara conquers a gold medal in the international six-day event in Sweden, racing as a private competitor. Still in 1966, next to the 125 comes the 150 (142.6 cc), followed one year later by the 100 cc (98.1 cc) when all the motorcycles are equipped with a new closed duplex cradle frame with a vertical printed sheet metal tube, built by the specialist Ronzani, together with new suspensions and an engine with modified power supply and transmission. This Corsaro allows Dossena, Collina and Gritti to win the Italian championships in the 100, 125 and 175 categories to which are added 4 gold medals in the six-days and 3 victories in the Bologna two-days event. During 1968 the engine is further developed. The larger engine size single-cylinder increases to 153.1 cc in order to have greater torque and power. Success continues both in Italy and abroad. The six-days race is held in San Pellegrino, 19 Moto Morini bikes are entered and 15 arrive at the finishing line winning 7 gold medals, 3 silver and 3 bronze. Rottigni wins the Bologna two-days event and the National Championship in the 100 category, while Collina takes the 125 class. In the winter of 1968-1969 the Ronzani frame is strengthened. The restyling includes its appearance with a different fuel tank (rapidly detachable), supplied with knee pads and a tool box panel, while the engine has a 5-speed gear change. Already in 1968 comes the refined Bonazzi & Gambetta fork including, and this is exclusive, breather cap valve vents. Among the 1969 results we recall the victories in the Italian Championship 100 category of Signorelli and Gritti in the 175. In 1970 Gritti again wins in the 175 and Oldrati in the 125. Moreover in the six-days in Spain the three Italian teams (one in the trophy and two in the vase) all race with Morini bikes, with updated engines, Ceriani forks and on 6 motorbikes the experimental Verlicchi frames. Of the 22 Corsaro entered, 19 finish the race with 16 gold medals, 2 silver and 1 bronze. These examples are the base of the new and final 1971 version which will, with the 125, be put alongside the 165 (163.9 cc) substituting the 160.",
+  },
+  {
+    date: "1975",
+    title: "1975",
+    body: "At the beginning of the year the Corsaro is substituted by the 125 h with an engine which is by and large derived from the 3 1⁄2. It is the most modern of its contemporary 4 strokes and boasts some unique details, from its electronic ignition to six gears. Moreover it immediately enjoys the benefits of a 260 mm front break disc, the same as on the 3 1⁄2. Having 13.75 HP at 9.000 rpm it can reach speeds of 125 km/h. The 125 goes out of production in 1985, leaving the KJ 125 endurance bike with the task of carrying the Morini flag among motorbikes for sixteen year olds.",
+  },
+  {
+    date: "1977",
+    title: "1977",
+    body: "From 1975 Morini works on the new 500 road bike, presented in its definitive version at the 1977 Milan trade fair. The engine size increases to 478.5 cc (69×64 mm), power to 43HP at 7.500 rpm, and speed to 175 km/h. A year later, working only on its external appearance, the sports version is created. In 1981 the engines are supplied with a six-speed gear change. 1978 sees the debut of the excellent 250 two-cylinder (59×43,8 mm = 239.5 cc) with 25 HP at 9.000 rpm and a speed of 140 km/h, which sells well, especially abroad. Below, the 1977 range which also includes the 18.5 HP single-cylinder 250.",
+  },
+  {
+    date: "1986",
+    title: "1986",
+    body: "After having acquired Ducati in 1985, Cagiva took another step and at the end of 1986 bought the brand and the factory in via Bergami from Gabriella Morini for 4.3 billion lire. Moto Morini has 86 employees, an annual revenue of 20 billion lire, no debt, and a good sales network and a new engine, ready to enter into production. With the arrival of the Cagiva Castiglioni brothers management “enthusiasm was still running high: we were preparing five prototypes of new models, among which an endurance bike, to show off our worth – recalls the designer Franco Lambertini – and also when they changed us into an engineering firm we were working with passion. Moreover the new 720 cc engine delivered 85 HP at 7.200 rpm and its production would have cost very little, in my opinion one billion lire less than a Ducati engine. It is my view Ducati were really afraid of this and they stopped us”.",
+  },
+  {
+    date: "1987",
+    title: "1987",
+    body: "In February 1987 the new engine production was stopped. That year, compared with 2,000 Morini bikes, one thousand Husqvarna and a similar number of Ducati were made. Then the company was transformed into an engineering firm, the assembly moved to Ducati until in 1990 the plant in via Bergami was closed and the production of custom models entrusted to Schiranna. In 1992 the last examples of the Excalibur and New York are mounted onto the Agostini Moto.",
+  },
+  {
+    date: "1999",
+    title: "1999",
+    body: "Motori Franco Morini, founded by Alfonso Morini’s nephew in 1954 with its headquarters in Casalecchio di Reno just outside Bologna, acquires the Moto Morini brand from Ducati, while spare parts and prototypes which are still stored in Borgo Panigale are sold to private individuals and collectors.",
+  },
+  {
+    date: "2003",
+    title: "2003",
+    body: "Maurizio Morini, Franco’s son and for many years the head of the company, involves the Berti family in the relauch project. And so Moto Morini Spa is founded with Lambertini as head of the technical department.",
+  },
+  {
+    date: "2005",
+    title: "2005",
+    body: "The year of the company relaunch. The new Corsaro 1200 (1.187 cc, 140 HP) and 9 1/2 (948 cc, 117 HP) motorcycles are an innovative Lambertini project, while the design is under the direction of Luciano Marabese. The new engine is a 87° V twin-cylinder, 4 valve twin-shaft, with electronic fuel injection and 6 gears and with a trellis tube frame.",
+  },
+  {
+    date: "2007",
+    title: "2007",
+    body: "Besides the Corsaro Veloce 1200, endowed with a more sophisticated chassis, the Scrambler 1200 and the Granpasso 1200 maxi endurance bikes make their debuts. The Morini family now has complete control of the company.",
+  },
+  {
+    date: "2010",
+    title: "2010",
+    body: "Having entered a period of crisis, Moto Morini is placed into voluntary liquidation before being declared bankrupt by the Court of Bologna on 17th May 2010. The bankruptcy liquidator is able to restart production with a limited number of Granpasso and Scrambler models being assembled using up warehouse stock. On 19th July 2011 Moto Morini is acquired by two entrepreneurs: Sandro Capotosti and Ruggeromassimo Jannuzzelli",
+  },
+  {
+    date: "2013",
+    title: "2013",
+    body: "With the ending of the rental agreement for the Casalecchio factory, Moto Morini relocates to the province of Pavia, to Trivolzio, just outside Milan.",
+  },
+  {
+    date: "2015",
+    title: "2015",
+    body: "The ownership passes entirely to the Jannuzzelli family. A development process then starts to ensure the motorcycles respect Euro 4 norms, following which new models are created, from which the bold Corsaro ZZ, Corsaro ZT and the fascinating Milano derive.",
+  },
+  {
+    date: "2018",
+    title: "2018",
+    body: "In October 2018 the company changes hands and becomes part of the Zhongneng Vehicle Group, which aims to consolidate and reinforce the Golden Eagle company’s prestigious positioning by making significant investments which concern both current and new models, also with different engine sizes with the objective of widening the Morini range and increasing its presence on the market.",
+  },
+];
---
+++ components/Timeline/Timeline.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Timeline.module.scss";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+const Timeline = ({ timelineItems }) => {
+  return (
+    <div className={styles.container}>
+      <VerticalTimeline>
+        {timelineItems.map((item) => (
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "#fff", color: "#000" }}
+            contentArrowStyle={{ borderRight: "7px solid  #fff" }}
+            date={item.date}
+            iconStyle={{ background: "#ff0000", color: "#fff" }}
+            key={item.date}
+          >
+            <h3 className="vertical-timeline-element-title">{item.title}</h3>
+            <p>{item.body}</p>
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+    </div>
+  );
+};
+
+export default Timeline;
