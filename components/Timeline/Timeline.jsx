d872b38351925ceca44fd4d67d42dd028dadef92
Adam Baker <adam_baker@live.co.uk>
2022-09-22 19:56:07 +0100

    FIX: fixed overflow on mobile devices due to the animation on heritage page

--- components/Timeline/Timeline.jsx
+++ components/Timeline/Timeline.jsx
@@ -10,30 +10,30 @@ import "react-vertical-timeline-component/style.min.css";
 
 const Timeline = ({ timelineItems }) => {
   return (
-    <div className={styles.container}>
-      <VerticalTimeline>
-        {timelineItems.map((item) => (
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "#fff", color: "#000" }}
-            contentArrowStyle={{ borderRight: "7px solid  #fff" }}
-            date={item.date}
-            iconStyle={{
-              background: "#fff",
-              color: "#ff0000",
-              justifyContent: "center",
-              alignItems: "center",
-              display: "flex",
-            }}
-            icon={<MoriniIcon />}
-            key={item.date}
-          >
-            <h3 className="vertical-timeline-element-title">{item.title}</h3>
-            <p>{item.body}</p>
-          </VerticalTimelineElement>
-        ))}
-      </VerticalTimeline>
-    </div>
+    // <div className={styles.container}>
+    <VerticalTimeline>
+      {timelineItems.map((item) => (
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "#fff", color: "#000" }}
+          contentArrowStyle={{ borderRight: "7px solid  #fff" }}
+          date={item.date}
+          iconStyle={{
+            background: "#fff",
+            color: "#ff0000",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+          }}
+          icon={<MoriniIcon />}
+          key={item.date}
+        >
+          <h3 className="vertical-timeline-element-title">{item.title}</h3>
+          <p>{item.body}</p>
+        </VerticalTimelineElement>
+      ))}
+    </VerticalTimeline>
+    // </div>
   );
 };
 
--- pages/heritage.jsx
+++ pages/heritage.jsx
@@ -6,7 +6,11 @@ import Timeline from "../components/Timeline/Timeline";
 import { timelineItems } from "../components/Timeline/Data";
 
 const Heritage = () => {
-  return <Timeline timelineItems={timelineItems} />;
+  return (
+    <div className={styles.container}>
+      <Timeline timelineItems={timelineItems} />;
+    </div>
+  );
 };
 
 export default Heritage;
